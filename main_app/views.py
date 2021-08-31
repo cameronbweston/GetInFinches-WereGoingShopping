813e363ece85b4458ac31fcebb33d4f481e3655c
Cameron Weston <cameronbweston@gmail.com>
2021-08-30 20:54:20 -0500

    Add a bunch of css and templates. Work on displaying bird in index page

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Finch
 
+def cats_index(request):
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', { 'finches': finches })
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Whats up Finches??</h1>')
-
+  return render(request, 'home.html')
 def about(request):
   return render(request, 'about.html')
