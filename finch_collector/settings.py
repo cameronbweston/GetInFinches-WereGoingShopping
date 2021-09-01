b6ef4deba26b0ee08b60c9b2155c4625502b1b40
Cameron Weston <cameronbweston@gmail.com>
2021-09-01 13:32:58 -0500

    Make changes to secret key in settigns

--- finch_collector/settings.py
+++ finch_collector/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-2249astit=74jk_k!=*lzotd%4_^1v_5w1b48j8%g^%ep-o0k='
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
